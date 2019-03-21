8f5291fc0e0c4c47833d4520be11fa0143bd0132
Soroush Khosravi <sorush.kh68@gmail.com>
2019-03-21 09:56:33 +0000

    We can now print the html string on the html file.

--- src/note-controller.js
+++ src/note-controller.js
@@ -1,5 +1,25 @@
 
-window.addEventListener('load', function() {
-  // console.log(document.getElementById("app").innerText);
-  document.getElementById("app").innerHTML ="soroush" ;
-})
+// window.addEventListener('load', function() {
+//   // console.log(document.getElementById("app").innerText);
+//   document.getElementById("app").innerHTML ="soroush" ;
+// })
+
+
+  window.addEventListener('load', function() {
+    var noteList = new NoteList(Note);
+    var controller = new Controller(noteList , "Favourite drink: seltzer");
+  // var controller = new Controller(noteList , "Favourite drink: seltzer");
+    var noteListView = new NoteListView(noteList)
+    document.getElementById("app").innerHTML = noteListView.getNoteString();
+
+
+
+    // console.log(document.getElementById("app").innerText);
+
+});
+
+function Controller(noteList,text){
+  this.noteList = noteList ;
+  this.text = text;
+  this.noteList.createNote(this.text);
+}
